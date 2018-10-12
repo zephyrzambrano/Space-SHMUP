dd89611dc13fbe1ac35a2421ddd09f3a3c336bfc
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-10-12 16:22:14 -0400

    The Hero Update() Method

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+	static public Hero S; //Singleton
+	
+	[Header("Set in Inspector")]
+	//these fields control the movement of the ship
+	public float speed=30;
+	public float rollMult=-45;
+	public float pitchMult=30;
+	
+	[Header("Set Dynamically")]
+	public float shieldLevel=1;
+	
+	void Awake() {
+		if (S==null) {
+			S=this; //set the Singleton
+		}
+		else {
+			Debug.LogError("Hero.Awake()=Attempted to assign second Hero.S!");
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//pull in information from the Input class
+		float xAxis=Input.GetAxis("Horizontal");
+		float yAxis=Input.GetAxis("Vertical");
 		
+		//change transform.position based on the axes
+		Vector3 pos=transform.position;
+		pos.x+=xAxis*speed*Time.deltaTime;
+		pos.y+=yAxis*speed*Time.deltaTime;
+		transform.position=pos;
+		
+		//rotate the ship to make it feel more dynamic
+		transform.rotation=Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult, 0);
 	}
 }
