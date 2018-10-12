b6e22c69b9379b2071f320e19ffa01b7e520b3e4
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-10-12 18:38:44 -0400

    The Hero Shield

--- Assets/_Scripts/Shield.cs
+++ Assets/_Scripts/Shield.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shield : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public float rotationsPerSecond = 0 / 1f;
+
+    [Header("Set Dynamically"]
+    public int levelShown = 0;
+
+    //this non-public variable will not appear in the Inspector
+    Material mat;
 
 	// Use this for initialization
 	void Start () {
-		
+        mat = GetComponent<Renderer>.material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //read the current shield level from the Hero Singleton
+        int currLevel = Mathf.FloorToInt(Hero.S.shieldLevel);
+        //if this is different from levelShown...
+        if (levelShown !=currLevel) {
+            levelShown = currLevel;
+            //adjust the texture offset to show different shield level
+            mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0);
+        }
+        //rotatte the shield a bit every frame in a time-based way
+        float rZ = -(rotationsPerSecond * Time.time * 360) % 360f;
+        transform.rotation=Quaternion.Euler(0, 0, rZ);
 	}
 }
