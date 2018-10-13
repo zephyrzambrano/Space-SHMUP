14d6458fe51fc7764bdd68ecc7b7d6214911405d
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-10-13 11:32:49 -0400

    Enemy Artwork & Started Enemy C# Script

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f; //the speed in m/s
+    public float fireRate = 0.3f; // seconds/shot (unused)
+    public float health = 10;
+    public int score = 100; // points earned for destroying this
+
+    //this is a Property: a method that acts like a field
+    public Vector3 pos {
+        get {
+            return (this.transform.position);
+        }
+        set {
+            this.transform.position = value;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Move();
+    }
+
+    public virtual void Move() {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
