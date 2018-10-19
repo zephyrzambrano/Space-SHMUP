0e11afed33e3763b14e1ffb6ad2c6e2c34b913a0
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-10-19 19:15:05 -0400

    Enemy_2

--- Assets/_Scripts/Enemy_2.cs
+++ Assets/_Scripts/Enemy_2.cs
@@ -2,15 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_2 : MonoBehaviour {
+public class Enemy_2 : Enemy {
+    [Header("Set in Inspector: Enemy_2")]
+    //determines how much the Sine wave will affect movement
+    public float sinEccentricity = 0.6f;
+    public float lifeTime = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Set Dynamically: Enemy_2")]
+    //Enemy_2 uses a Sin wave to modify a 2-point linear interpolation
+    public Vector3 p0;
+    public Vector3 p1;
+    public float birthTime;
+
+    // Use this for initialization
+    void Start() {
+        //pick any point on the left side of the screen
+        p0 = Vector3.zero;
+        p0.x = -bndCheck.camWidth - bndCheck.radius;
+        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        //pick any point on the right side of the screen
+        p1 = Vector3.zero;
+        p1.x = bndCheck.camWidth + bndCheck.radius;
+        p1.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+
+        //possibly swap sides
+        if (Random.value > 0.5f) {
+            //setting the .x of each point to its negative will move it to
+            //the other side of the screen
+            p0.x *= -1;
+            p1.x *= -1;
+        }
+
+        //set the birthTime to the current time
+        birthTime = Time.time;
+    }
+
+    public override void Move() {
+        //Bezier curves work based on a u value between 0 and 1
+        float u = (Time.time - birthTime) / lifeTime;
+        //if u>1, then it has been longer than lifeTime since birthTime
+        if (u > 1) {
+            //this Enemy_2 has finished its life
+            Destroy(this.gameObject);
+            return;
+        }
+
+        //adjust u by adding a U Curve based on a Sine wave
+        u = u + sinEccentricity * (Mathf.Sin(u * Mathf.PI * 2));
+
+        //interpolate the two linear interpolation points
+        pos = (1 - u) * p0 + u * p1;
+    }
 }
