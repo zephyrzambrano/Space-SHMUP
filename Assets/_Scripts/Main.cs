4cfadde79c0f1370dcea0971c1c012bf9d91da17
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-10-19 16:05:27 -0400

    Spawning Enemies at Random

---
+++ Assets/_Scripts/Main.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //for loading & reloading of scenes
+
+public class Main : MonoBehaviour {
+    static public Main S; //a singleton for Main
+
+    [Header("Set in Inspector")]
+    public GameObject[] prefabEnemies; //array of Enemy prefabs
+    public float enemySpawnPerSecond = 0.5f; //number of Enemies/second
+    public float enemyDefaultPadding = 1.5f; //padding for position
+
+    private BoundsCheck bndCheck;
+
+    void Awake() {
+        S = this;
+        //set bndCheck to reference the BoundsCheck component on this GameObject
+        bndCheck = GetComponent<BoundsCheck>();
+        //invoke SpawnEnemy() once (in 2 seconds, based on default values)
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy() {
+        //pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        //position the Enemy above the screen with a random x position
+        float enemyPadding = enemyDefaultPadding;
+        if (go.GetComponent<BoundsCheck>() != null) {
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        //set the initial posotion for the spawned Enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyPadding;
+        float xMax = bndCheck.camWidth - enemyPadding;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyPadding;
+        go.transform.position = pos;
+
+        //Invoke SpawnEnemy again
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
