65577cd15c0c50c510558c37c60f9953db424e5b
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-10-19 19:38:51 -0400

    The WeaponType Enum

---
+++ Assets/_Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is an enum of the various possible weapon types.
+/// It also includes a *shield* type to allow a shield power-up.
+/// Items marked [NI] below are Not Implimented in the IGDPD book.
+/// </summary>
+
+public enum WeaponType {
+    none,       // the default/no weapon
+    blaster,    // a simple blaster
+    spread,     // two shots simultaneously
+    phaser,     // [NI] shots that move in waves
+    missile,    // [NI] homing missiles
+    laser,      // [NI] damage over time
+    shield      // raise shieldLevel
+}
+
+public class Weapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
