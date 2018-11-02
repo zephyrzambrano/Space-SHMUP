44dd5f84b94d36866c1097d0732df3faf4351fd2
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-11-02 19:20:04 -0400

    Fixed Blaster and Spread Firing/Started Movement of Enemy_4

--- Assets/_Scripts/Enemy_4.cs
+++ Assets/_Scripts/Enemy_4.cs
@@ -2,15 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_4 : MonoBehaviour {
+/// <summary>
+/// Enemy_4 will start offscreen and then pick a random point on screen to
+/// move to. Once it has arrived, it will pick another random point and
+/// continue until the player has shot it down.
+/// </summary>
+
+public class Enemy_4 : Enemy {
+
+    private Vector3 p0, p1; // The two points to interpolate
+    private float timeStart; // Birth time for this Enemy_4
+    private float duration = 4; // Duration of movement
 
 	// Use this for initialization
 	void Start () {
-		
+        // There is already an initial position chosen by Main.SpawnEnemy()
+        // so add it to points as the initial p0 & p1
+        p0 = p1 = pos;
+
+        InitMovement();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void InitMovement() {
+        p0 = p1; // Set p0 to the old p1
+        // AAssign a new on-screen location to p1
+        float widMinRad = bndCheck.camWidth - bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+        p1.x = Random.Range(-widMinRad, widMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        // Reset the time
+        timeStart = Time.time;
+    }
+
+    public override void Move() {
+        // 
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
